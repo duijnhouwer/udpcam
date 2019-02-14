2b604bd6c8aaa8d50984f81b3f3c2ef7dde134c1
duijnhouwer <j.duijnhouwer@gmail.com>
2019-02-14 17:35:27 -0500

    VideoWriterGui now works, todo: try rewrite as regular function with global variables instead of class to make output #1  the video object (instead of #2). force proper extension to filename, too

--- VideoWriterGui.m
+++ VideoWriterGui.m
@@ -14,7 +14,7 @@ classdef VideoWriterGui < handle
             O.prof_info=ProfileFactory.getKnownProfiles;
             
             if ~exist('filename','var') || isempty(filename)
-                filename=fullfile(pwd,['video_' datestr(now,'YYYYMMDD_hhmmss')]);
+                filename=fullfile(pwd,datestr(now,'YYYYMMDD_hhmmss'));
             elseif ~ischar(filename)
                 error('arg #1 (filename) must be empty or a string');
             end
@@ -30,8 +30,10 @@ classdef VideoWriterGui < handle
             
             % Create the main window
             O.fig_obj=figure;
-            O.fig_obj.Position=p.Results.position;
-            O.fig_obj.Visible='off';
+            clf(O.fig_obj);
+            O.fig_obj.Color=[0.94 0.94 0.94];
+            %O.fig_obj.Position=p.Results.position;
+          %  O.fig_obj.Visible='off';
             O.fig_obj.Units='Pixels';
             O.fig_obj.CloseRequestFcn=@O.close_button_callback;
             O.fig_obj.ResizeFcn=[];
@@ -39,15 +41,15 @@ classdef VideoWriterGui < handle
             O.fig_obj.MenuBar='none';
             O.fig_obj.ToolBar='none';
             O.fig_obj.Name = mfilename;
+        
+            % Add the static GUI elements
+            O.build_gui(filename,profile);
             % Put at center of screen if requested
             if p.Results.center
                 movegui(O.fig_obj,'center')
             end
-            
-            % Add the static GUI elements
-            O.build_gui(filename,profile)
             % Make the window visible.
-            fig_obj.Visible = 'on';
+            O.fig_obj.Visible = 'on';
             drawnow;
             uiwait(O.fig_obj);
             vid_obj=O.vid_obj;
@@ -55,24 +57,40 @@ classdef VideoWriterGui < handle
         end
         
         function build_gui(O,filename,profile)
+            
+            persistent oldfilename oldprofile
+            
+            oldfilename
+            filename
+            oldprofile
+            profile
+            
+            if strcmpi(oldfilename,filename) && strcmpi(oldprofile,profile) 
+                return
+            end
+            oldfilename=filename;
+            oldprofile=profile;
+            
+            disp('build_gui');
+            
             % all dimension in pixels
-            lblw=60;
-            hei=18;
-            vspace=12;
+            leftcolwid=140;
+            rightcolwid=140;
+            hei=20;
+            vspace=6;
             hspace=10;
-            row=hei+vspace;
-            top=10;
+            rowhei=hei+vspace;
+            bottom=10;
             left=10;
             
-            % Determine the number of rows in the gui
-            n_rows=3;
-            % set the window height accordingly
-            fig_hei=n_rows*(row+vspace);
-            O.fig_obj.Position(4)=fig_hei;
-            % Add file search button
+            % Clear all uicontrols
+            delete(O.fig_obj.Children)
+            clf(O.fig_obj);
+            
+            
+            % Add a file search button
             filebut=uicontrol('Parent',O.fig_obj,'Style','pushbutton','String','File');
             filebut.Tag='filebut';
-            filebut.Position=[left fig_hei-1*row lblw hei];
             filebut.Callback=@O.filebut_callback;
             if exist('filename','var')
                 filebut.TooltipString=filename;
@@ -84,46 +102,92 @@ classdef VideoWriterGui < handle
             if exist('profile','var')
                 profpop.Value=find(strcmpi(profile,{O.prof_info(:).Name}));
             end
-            profpop.Position=[left+lblw+hspace fig_hei-row 140 hei];
             profpop.Callback=@O.profpop_callback;
             profpop.TooltipString='';
             profpop.Callback=@O.profpop_callback;
             profpop.ButtonDownFcn=@O.profpop_callback;
-            O.profpop_callback;
+            profpop.TooltipString=O.prof_info(profpop.Value).Description;
+
             
             % vid_obj=VideoWriter(filebut.TooltipString,profpop.String{profpop.Value});
+            % Make the video object, which, depending on the profile used
+            % will have a number of settable properties
+            O.make_vid_obj;
+            propnames=fieldnames(settable_properties(O.vid_obj));
+            propvals=struct2cell(settable_properties(O.vid_obj));
+            % add uicontrols for the settable properties
             
-            
-            
+            for i=1:numel(propnames)
+                % The property name field
+                uicontrol('Parent',O.fig_obj,'Style','text','String',propnames{i});
+                % The corresponding value
+                if ~isempty(propvals{i})
+                    valstr=num2str(propvals{i}); % num2str('asd') > 'asd'
+                else
+                    valstr='[]';
+                end
+                uicontrol('Parent',O.fig_obj,'Style','edit','String',valstr,'Tag',['value_' propnames{i}]);
+            end
+                
             % Add Cancel button
             cancelbut=uicontrol('Parent',O.fig_obj,'Style','pushbutton','String','Cancel');
             cancelbut.Tag='cancelbut';
-            cancelbut.Position=[left fig_hei-2*row lblw hei];
             cancelbut.Callback=@O.cancelbut_callback;
             % Add OK button
             okbut=uicontrol('Parent',O.fig_obj,'Style','pushbutton','String','OK');
             okbut.Tag='okbut';
-            okbut.Position=[left+lblw+hspace fig_hei-2*row lblw hei];
-            okbut.Callback=@O.okbut_callback;
+            okbut.Callback=@O.okbut_callback; 
             
-            % Apply style to all elements
-            %style={'BackgroundColor',fig_obj.Color,'FontSize',10};
-            style={'FontSize',10};
-            ui_elements=findobj(O.fig_obj.Children,'Type','UIControl');
-            for i=1:numel(ui_elements)
-                set(ui_elements(i),style{:});
+           % set the window width and height to accomodate all uicontrols
+           all_ui=findobj(O.fig_obj,'Type','UIControl');
+           O.fig_obj.Position(3)=3*hspace+leftcolwid+rightcolwid;
+           oldhei=O.fig_obj.Position(4);
+           O.fig_obj.Position(4)=ceil(numel(all_ui)/2)*rowhei+2*vspace;
+           O.fig_obj.Position(2)=O.fig_obj.Position(2)+oldhei-O.fig_obj.Position(4);
+            % Set the position and size of all uicontrols
+            for i=1:2:numel(all_ui)
+                all_ui(i).Position=[left+leftcolwid+hspace bottom+floor(i/2)*rowhei rightcolwid hei];
+                all_ui(i+1).Position=[left bottom+floor(i/2)*rowhei leftcolwid hei];
             end
+            % Cosmetically adjust button and popup menu heights
+            for i=1:numel(all_ui)
+                if strcmpi(all_ui(i).Style,'pushbutton')
+                    all_ui(i).Position(2)=all_ui(i).Position(2)-hei*0.2;
+                    all_ui(i).Position(4)=round(hei*1.25);
+                elseif strcmpi(all_ui(i).Style,'popupmenu')
+                    all_ui(i).Position(2)=all_ui(i).Position(2)+hei*0.2;
+                    all_ui(i).Position(4)=round(hei/1.2);
+                end
+            end          
+            drawnow;
         end
         function okbut_callback(O,~,~)
+            if O.make_vid_obj
+                delete(O.fig_obj);
+            end
+        end
+        function ok=make_vid_obj(O)
+            ok=true;
             filebut=findobj(O.fig_obj,'Tag','filebut');
             profpop=findobj(O.fig_obj,'Tag','profpop');
             try
+                delete(O.vid_obj);
                 O.vid_obj=VideoWriter(filebut.TooltipString,profpop.String{profpop.Value});
+                paramcontrols=O.fig_obj.Children(startsWith({O.fig_obj.Children.Tag},'value_'));
+                for i=1:numel(paramcontrols)
+                    paramname=paramcontrols(i).Tag(numel('value_1'):end);
+                    newval=eval(strtrim(paramcontrols(i).String));
+                    if ~all(O.vid_obj.(paramname)==newval)
+                        % only setting when changed prevents "compression
+                        % ratio" cant be set when lossless compression is
+                        % true for Archival profile
+                        O.vid_obj.(paramname)=eval(paramcontrols(i).String);
+                    end
+                end
             catch me
                 uiwait(errordlg(me.message,mfilename,'modal'));
-                return;
+                ok=false;
             end
-            delete(O.fig_obj);
         end
         function cancelbut_callback(O,~,~)
             O.vid_obj=[];
@@ -134,6 +198,7 @@ classdef VideoWriterGui < handle
         end
         
         function profpop_callback(O,~,~)
+            persistent previous_profile
             profpop=findobj(O.fig_obj,'Tag','profpop');
             profpop.TooltipString=O.prof_info(profpop.Value).Description;
             % change the extension of the filename to the selected profile
@@ -142,7 +207,12 @@ classdef VideoWriterGui < handle
             ext=[O.prof_info(:).FileExtensions];
             fname=fullfile(fld,nme);
             filebut.TooltipString=[fname ext{profpop.Value}];
-          % O.build_gui
+            %
+            if ~strcmpi(previous_profile,profpop.String{profpop.Value})
+                previous_profile=profpop.String{profpop.Value};
+                O.build_gui(filebut.TooltipString,profpop.String{profpop.Value});
+            end
+            
         end
         function filebut_callback(O,~,~)
             filebut=findobj(O.fig_obj,'Tag','filebut');
