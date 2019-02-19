16f9e7297f0092d60fb7ed52a4905e09819b85cb
duijnhouwer <j.duijnhouwer@gmail.com>
2019-02-18 19:23:44 -0500

    Numerous changes to udpcam_class:
    Intermediate file can not be specified, only one file is specified with the Output>VideoWriter.  Framerate of this VideoWriter is used for resampling
    automatic resampling of the video file is now an option (output>resample)
    now saves timestamp file (CSV format, remember application will be a standalone window executable)
    detect keypresses, currently double-escape can be used to cancel the file resampling
    noise screen now uses latest frame size (pixels) instead of hardcoded 320x240
    UDP panel look a little better
    Fixed a bug where Colorspace was reset to RGB when recording started (because was only stored in menu, which is gone when rec starts)

--- udpcam_class.m
+++ udpcam_class.m
@@ -8,18 +8,18 @@ classdef udpcam_class < handle
         cam_obj
         vid_obj
         exit_flag=false
-        rec_frames=0 % Dual purpose flow-control flag AND counter for frame_timestamp_s array
+        rec_frames=0 % Dual purpose flow-control flag AND counter for frame_grab_s array
         mainMenu
         display
         frame
         rec_start_tic
-        frame_timestamp_s
-        %intermediate % stores filename of intermediate video file streamed to disk
-        %final % stores filename of final, resampled video file
-        %delete_intermediate=true;
+        last_key_press
+        frame_grab_s
         video_profile
         overlay
         win_resize_tic % timer since last window resize
+        color_space;
+        resample_after_rec; % make video realtime after recording
     end
     
     methods (Access=public)
@@ -37,18 +37,23 @@ classdef udpcam_class < handle
             O.exit_flag=false;
             O.rec_frames=0;
             O.rec_start_tic=tic;
+            O.last_key_press=[];
             
             % Set up the UDP connection for receiving commands and sending
             % feedback
-            O.udp_settings.enable=true;
-            O.udp_settings.remotehost=p.Results.IP;
-            O.udp_settings.localport=p.Results.LocalPort;
-            O.udp_settings.remoteport=p.Results.RemotePort;
+            O.udp_settings.Enable=true;
+            O.udp_settings.RemoteHost=p.Results.IP;
+            O.udp_settings.LocalPort=p.Results.LocalPort;
+            O.udp_settings.RemotePort=p.Results.RemotePort;
             O.setup_udp_connection;
             
             % Set the video defaults
             O.video_profile='Archival';
             O.vid_obj=VideoWriter(fullfile(pwd,'vid.mj2'),O.video_profile);
+            % store color_space as class scope variable because we need
+            % access when menu is changed into "stop recording" only, too
+            O.color_space = categorical({'RGB'});
+            O.resample_after_rec = true;
             
             % Setup the window
             O.fig_obj=figure;
@@ -63,6 +68,8 @@ classdef udpcam_class < handle
             O.fig_obj.MenuBar='none';
             O.fig_obj.ToolBar='none';
             O.fig_obj.Name = [mfilename ' ' p.Results.IP ':' num2str(p.Results.LocalPort)];
+            O.fig_obj.WindowKeyPressFcn=@O.key_press_callback;
+            
             % Create the axes where the preview will be displayed
             O.axs_obj = axes(O.fig_obj);
             O.axs_obj.Units='normalized';
@@ -123,6 +130,16 @@ classdef udpcam_class < handle
             %   O.clean_up;
         end
         
+        function key_press_callback(O,~,evt)
+            s=toc(O.rec_start_tic); % second since rec start
+            if strcmp(evt.Key,'escape') && ~isempty(O.last_key_press) && strcmp(O.last_key_press.key,'escape') && s-O.last_key_press.s<1/3
+                O.last_key_press.key='escape-escape'; % detect twice escape within 1/3 of second
+            else
+                O.last_key_press.key=evt.Key;
+            end
+            O.last_key_press.s=s;
+        end
+        
         function setup_udp_connection(O)
             if ~isempty(O.udp_obj)
                 fclose(O.udp_obj);
@@ -130,7 +147,7 @@ classdef udpcam_class < handle
                 O.udp_obj=[];
             end
             try
-                O.udp_obj=udp(O.udp_settings.remotehost,'RemotePort',O.udp_settings.remoteport,'LocalPort',O.udp_settings.localport);
+                O.udp_obj=udp(O.udp_settings.RemoteHost,'RemotePort',O.udp_settings.RemotePort,'LocalPort',O.udp_settings.LocalPort);
                 O.udp_obj.DatagramReceivedFcn = @O.parse_message;
                 fopen(O.udp_obj);
                 fprintf(O.udp_obj,'%s online',mfilename);
@@ -178,11 +195,12 @@ classdef udpcam_class < handle
             delete(O.mainMenu);
             O.mainMenu = uicontextmenu;
             if ~O.rec_frames>0
-                uimenu('Parent',O.mainMenu,'Label','UDP Settings...','Callback',@O.edit_udp_connection);
+                uimenu('Parent',O.mainMenu,'Label','UDP...','Callback',@O.edit_udp_connection);
                 uimenu('Parent',O.mainMenu,'Label','Camera');
-                uimenu('Parent',O.mainMenu,'Label','Video Settings...','Callback',@O.edit_output_settings);
+                uimenu('Parent',O.mainMenu,'Label','Output');
                 uimenu('Parent',O.mainMenu,'Label','Record','Separator','on','Callback',@O.start_recording);
                 O.build_camera_menu;
+                O.build_output_menu;
             else
                 uimenu('Parent',O.mainMenu,'Label','Stop Recording','Callback',@O.stop_recording);
             end
@@ -198,7 +216,7 @@ classdef udpcam_class < handle
             for i=1:numel(cams)
                 uimenu('Parent',selectMenu,'Label',cams{i},'Callback',@(src,evt)O.select_camera(src,evt));
             end
-            uimenu('Parent',selectMenu,'Label','Refresh List','Callback',@O.build_camera_menu);
+            uimenu('Parent',selectMenu,'Label','Refresh List','Callback',@O.build_camera_menu,'Separator','on');
             if ~isempty(O.cam_obj) && isvalid(O.cam_obj)
                 O.select_camera(findobj(selectMenu.Children,'flat','Label',O.cam_obj.Name));
             else
@@ -206,13 +224,12 @@ classdef udpcam_class < handle
             end
         end
         
-        %function build_output_menu(O,~,~)
-        %    outMenu=findobj(O.mainMenu.Children,'flat','Label','Output');
-        %    delete(outMenu.Children);
-        %    uimenu('Parent',outMenu,'Label','Intermediate File...','Callback',{@O.edit_output_filename,'intermediate'});
-        %    uimenu('Parent',outMenu,'Label','Final File...','Callback',{@O.edit_output_filename,'final'});
-        %    uimenu('Parent',outMenu,'Label','Settings...','Callback',@O.edit_output_settings);
-        %end
+        function build_output_menu(O,~,~)
+            outMenu=findobj(O.mainMenu.Children,'flat','Label','Output');
+            delete(outMenu.Children);
+            uimenu('Parent',outMenu,'Label','VideoWriter...','Callback',@O.edit_output_settings);
+            uimenu('Parent',outMenu,'Label','Resample','Checked',O.onoff(O.resample_after_rec),'Callback',@O.toggle_resample);
+        end
         
         function select_camera(O,src,~)
             cameraName=src.Text;
@@ -249,7 +266,7 @@ classdef udpcam_class < handle
                 for i=1:numel(spaces)
                     uimenu('Parent',colorMenu,'Label',spaces{i},'Callback',@O.select_color);
                 end
-                set(findobj(colorMenu.Children,'flat','Label',spaces{1}),'Checked','on')
+                set(findobj(colorMenu.Children,'flat','Label',O.color_space),'Checked','on')
             end
         end
         
@@ -262,9 +279,15 @@ classdef udpcam_class < handle
             end
         end
         
-        function select_color(~,src,~)
+        function select_color(O,src,~)
             set(src.Parent.Children,'Checked','off');
             src.Checked='on';
+            O.color_space=categorical({src.Text}); 
+        end
+        
+        function toggle_resample(O,src,~)
+            O.resample_after_rec=~O.resample_after_rec;
+            src.Checked=O.onoff(O.resample_after_rec);
         end
         
         function edit_udp_connection(O,~,action)
@@ -272,7 +295,9 @@ classdef udpcam_class < handle
             switch O.kindof(action)
                 case 'GUI'
                     while true
-                        [tmpset,pressedOk]=guisetstruct(tmpset,'UDP Settings',8);
+                        tit='UDP Settings';
+                        tit(end+1:end+42-numel(tit))=' ';
+                        [tmpset,pressedOk]=guisetstruct(tmpset,tit,8);
                         if ~pressedOk
                             return; % user changed their mind, no changes will be made
                         end
@@ -293,16 +318,16 @@ classdef udpcam_class < handle
             %
             function errstr=check_for_errors(set)
                 errstr='';
-                if ~islogical(set.enable) && ~any(set.enable==[1 0])
-                    errstr=sprintf('%s\n%s',errstr,'enable must be true or false or 1 or 0');
+                if ~islogical(set.Enable) && ~any(set.Enable==[1 0])
+                    errstr=sprintf('%s\n%s',errstr,'Enable must be true or false or 1 or 0');
                 end
-                if ~ischar(set.remotehost)
-                    errstr=sprintf('%s\n%s',errstr,'remotehost must be a string');
+                if ~ischar(set.RemoteHost)
+                    errstr=sprintf('%s\n%s',errstr,'RemoteHost must be a string');
                 end
-                if ~isnumeric(set.localport)
+                if ~isnumeric(set.LocalPort)
                     errstr=sprintf('%s\n%s',errstr,'port must be a number');
                 end
-                if ~isnumeric(set.remoteport)
+                if ~isnumeric(set.RemotePort)
                     errstr=sprintf('%s\n%s',errstr,'port must be a number');
                 end
             end
@@ -382,29 +407,38 @@ classdef udpcam_class < handle
         end
         
         function grab_frame(O)
+            persistent last_size
+            if isempty(last_size)
+                last_size=[240 320];
+            end
             if ~isempty(O.cam_obj) && isvalid(O.cam_obj) && ~O.exit_flag
                 try
                     O.frame=O.cam_obj.snapshot;
+                    last_size=size(O.frame);
                 catch me
                     % cam_obj.snapshot will throw a timeout error if a settings
                     % dialog has been open. Catch that error here and set f to
-                    % some value
-                    disp(['grab_frame - ' me.message])
-                    O.frame=repmat(randi(256,240,320,'uint8')-1,1,1,3);
+                    % some noise values disp(['grab_frame - ' me.message])
+                    O.frame=repmat(randi([0 255],last_size(1:2),'uint8'),1,1,3);
                 end
             else
-                O.frame=repmat(randi(256,240,320,'uint8')-1,1,1,3);
+                O.frame=repmat(randi([0 255],last_size(1:2),'uint8'),1,1,3);
             end
-            switch O.get_color_space(O.mainMenu)
-                case 'RGB', O.frame=O.frame;
-                case 'Grayscale', O.frame=repmat(rgb2gray(O.frame),1,1,3);
-                case 'R', O.frame(:,:,[2 3])=0;
-                case 'G', O.frame(:,:,[1 3])=0;
-                case 'B', O.frame(:,:,[1 2])=0;
-                otherwise, error('Unknown colorspace: %s',color_space)
+            if O.color_space=='RGB' %#ok<*BDSCA>
+                O.frame=O.frame;
+            elseif O.color_space=='Grayscale'
+                O.frame=repmat(rgb2gray(O.frame),1,1,3);
+            elseif O.color_space=='R'
+                O.frame(:,:,[2 3])=0;
+            elseif O.color_space=='G'
+                O.frame(:,:,[1 3])=0;
+            elseif O.color_space=='B'
+                O.frame(:,:,[1 2])=0;
+            else
+                error('Unknown colorspace: %s',O.color_space)
             end
             if O.rec_frames>0
-                O.frame_timestamp_s(O.rec_frames)=toc(O.rec_start_tic);
+                O.frame_grab_s(O.rec_frames)=toc(O.rec_start_tic);
                 O.rec_frames=O.rec_frames+1;
             end
         end
@@ -480,7 +514,6 @@ classdef udpcam_class < handle
         
         function start_recording(O,~,~)
             try
-                %      O.vid_obj=VideoWriter(O.intermediate.filename,'Archival');
                 open(O.vid_obj);
             catch me
                 msg={sprintf('Could not open %s for saving video',O.vid_obj.Filename)};
@@ -488,10 +521,11 @@ classdef udpcam_class < handle
                 uiwait(errordlg(msg,mfilename,'modal'));
                 return
             end
-            O.frame_timestamp_s=[];
+            O.frame_grab_s=[];
             O.rec_frames=1; % flag *and* counter
             O.build_main_menu; % changed to "stop recording" only when rec_frames>0
             O.rec_start_tic=tic;
+            O.last_key_press=[];
         end
         
         function stop_recording(O,~,~)
@@ -500,10 +534,30 @@ classdef udpcam_class < handle
             pause(0.1);
             close(O.vid_obj);
             if was_rec
-                O.show_frame; % to remove red border
-               % overlay_props=settable_properties(O.overlay);
-               % resample_video(O.intermediate.filename,O.frame_timestamp_s,O.final.filename,'MPEG-4','FrameRate',100,'progfun',@O.show_resample_prog);%O.video_settings
-               % O.build_overlay(overlay_props); % restore overlay to values before resampling changed them
+                % Remove the red border
+                O.show_frame;
+                % Save the timestamps in separate file (considered storing
+                % them in top-left corner pixel of each frame but would
+                % only work with no or lossless compression
+                [~,fname]=fileparts(O.vid_obj.Filename); % remove the extension
+                timestampfile=fullfile(O.vid_obj.Path,[fname '_timestamps.txt']);
+                fid = fopen(timestampfile,'wt'); % Write Text mode lest \n doesn't work in Windows
+                try
+                    fprintf(fid,'%f\n',O.frame_grab_s);
+                    fclose(fid);
+                catch
+                    warning('Could not save timestamp file ''%s''!',timestampfile)
+                end
+                if O.resample_after_rec
+                    overlay_props=settable_properties(O.overlay);
+                    src=fullfile(O.vid_obj.Path,O.vid_obj.Filename);
+                    tstamps=O.frame_grab_s;
+                    fps=O.vid_obj.FrameRate;
+                    prof=O.video_profile;
+                    opts=settable_properties(O.vid_obj);
+                    resample_video(src,tstamps,fps,prof,'progfun',@O.show_resample_prog,'vidprops',opts);
+                    O.build_overlay(overlay_props); % restore overlay to values before resampling changed them
+                end
             end
             O.build_main_menu;
         end
@@ -520,14 +574,18 @@ classdef udpcam_class < handle
             end
         end
         
-        function show_resample_prog(O,i,ntotal)
+        function canceled_by_user=show_resample_prog(O,i,ntotal)
             persistent previous_percent
+            if i==1
+                O.last_key_press=[];
+            end
             now_percent=ceil(i/ntotal*100);
             if i==1 || i==ntotal || now_percent>=previous_percent+1
                 percent_str=sprintf('%d%%\n',now_percent);
                 O.overlay.String={'Resampling video...',percent_str};
                 previous_percent=now_percent;
             end
+            canceled_by_user=~isempty(O.last_key_press) && strcmp(O.last_key_press.key,'escape-escape');   
         end
         
         function build_overlay(O,restore_struct)
@@ -565,20 +623,6 @@ classdef udpcam_class < handle
     end
     
     methods (Static)
-        function str = get_color_space(mainMenu)
-            try
-                colmenu=findobj(mainMenu.Children,'Label','Color Space');
-                if isempty(colmenu)
-                    str='RGB';
-                else
-                    check_item=findobj(colmenu.Children,'flat','Checked','on');
-                    str=check_item.Text; % e.g. 'Grayscale'
-                end
-            catch me
-                disp(['get_color_space - ' me.message]);
-                str='RGB';
-            end
-        end
         function settings_struct=parse_assignment_string(settings_struct,assignstr)
             % if assignstr 'someparameter = 1'
             assignstr=strtrim(strsplit(assignstr,'='));
@@ -601,6 +645,13 @@ classdef udpcam_class < handle
                 nomstr=categorical({'UDP'});
             else
                 error('action should be ActionData or an assignstr');
+            end
+        end
+        function str=onoff(bool)
+            if bool
+                str='on';
+            else
+                str='off';
             end
         end
     end
