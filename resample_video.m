62cf636d2f909c2d1ed6c56c6a93261b354db1d2
duijnhouwer <j.duijnhouwer@gmail.com>
2019-02-18 19:13:25 -0500

    overhauled resample_video, now works with inputparser. handles infile same as outfile by overwriting the infile. now deletes temporary file. handles user_canceled signal from custom progfun

--- resample_video.m
+++ resample_video.m
@@ -1,58 +1,98 @@
-function resample_video(infile,tstamps,outfile,outprof,varargin)
+function resample_video(infile,tstamps,fps,profile,varargin)
     
     % Nearest neighbor temporal resampling of video file
-
+    % If the infile is empty or the same as the outfile, the conversion
+    % will first stream to an intermediate file which will at end replace
+    % the infile. The intermediate file will then be replaced.
+    
+    p=inputParser;
+    p.addRequired('infile',@ischar);
+    p.addRequired('tstamps',@isnumeric); % frame times in seconds
+    p.addRequired('fps',@(x)and(isnumeric(x),x>0)); % output frames per seconds
+    p.addRequired('profile',@ischar);
+    p.addParameter('out','',@ischar);
+    p.addParameter('vidprops',struct,@isstruct);
+    p.addParameter('progfun','',@(x)isa(x,'function_handle'));
+    p.parse(infile,tstamps,fps,profile,varargin{:});
+   
     if ~all(diff(tstamps)>0)
-        error('tstamps must be monotonically increasing');
+        error('frame times array in seconds (arg #2) must be increasing monotonically');
     end
     
-    [progfun,varargin]=skim(varargin,'progfun',@(x)isa(x,'function_handle'));
+    fin=VideoReader(infile);
     
-    out=VideoWriter(outfile,outprof);
-    tmpfile='';
-    if strcmpi(out.Filename,infile)
+    outfile=p.Results.out;
+    if isempty(outfile)
+        outfile=infile; % overwrite
+    end
+
+    if strcmpi(infile,outfile)
         % The output file has the same name as the input file. This means
         % that the inputfile needs to be replaced. Make up a temporary name
         % for the output now, and afterwards delete the inputfile and
         % rename the temporary file
-        tmpfile=[tempname(out.Path) '_' mfilename];
-        out=VideoWriter(tmpfile,outprof);
+        folder=fileparts(infile);
+        if isempty(folder)
+            folder=pwd;
+        end
+        tmpfile=[tempname(folder) '_' mfilename];
+        fout=VideoWriter(tmpfile,profile);
+    else
+        fout=VideoWriter(outfile,profile);
     end
-    in=VideoReader(infile);
+    % Se the optional video properties from vidprops struct. which are
+    % valid depends on the profile in use. MPEG-4 has Quality for example.
+    % All have FrameRate, but see below
+    flds=fieldnames(p.Results.vidprops);
+    for i=1:numel(flds)
+        if fout.(flds{i})~=p.Results.vidprops.(flds{i})
+            % only write when different to prevent error like 'Setting the
+            % CompressionRatio when LosslessCompression is enable is not
+            % allowed.'
+            fout.(flds{i})=p.Results.vidprops.(flds{i});
+        end
+    end
+    % All video profiles have the settable property FrameRate. If it was in
+    % vidprops it will now be overruled by required argumen fps
+    fout.FrameRate=fps;
     
-    if ~mod(numel(varargin),2)==0
-        error('The number of remaining arguments to pass to VideoWriter should be name-value pairs, but their number is odd.');
-    end
-    for i=1:2:numel(varargin)
-        out.(varargin{i})=varargin{i+1}; % for example 'Quality',100
-    end
-    
-    open(out);
+    % Start writing the frames to out
+    open(fout);
     try
-        n_out_frames=round(tstamps(end)*out.FrameRate);
-        nearest_frame=readFrame(in);
+        % withing try-catch so we can close(out) upon error
+        n_out_frames=round(tstamps(end)*fout.FrameRate);
+        nearest_frame=readFrame(fin);
         in_frame_counter=1;
+        canceled_by_user=false;
         for i=1:n_out_frames
-            feval(progfun,i,n_out_frames);
-            [~,nearest_neighbor]=min(abs(tstamps-i/out.FrameRate));
+            canceled_by_user=feval(p.Results.progfun,i,n_out_frames);
+            if canceled_by_user
+                break
+            end
+            [~,nearest_neighbor]=min(abs(tstamps-i/fout.FrameRate));
             if in_frame_counter<nearest_neighbor
                 in_frame_counter=nearest_neighbor;
-                nearest_frame=readFrame(in); % get the next frame
+                nearest_frame=readFrame(fin); % get the next frame
             end
-            writeVideo(out,nearest_frame); % add nearest frame to the output
+            writeVideo(fout,nearest_frame); % add nearest frame to the output
         end
     catch me
-        close(out)
+        close(fout)
         rethrow(me)
     end
-    close(out);
+    close(fout);
+    delete(fin); % free the handle (otherwise can't move file on top of it)
     
     % if the infile and the outfile were the same, replace original with
-    % the resampled one
-    if ~isempty(tmpfile)
+    % the resampled one. unless user canceled, then just delete it.
+    tmpfile=fullfile(fout.Path,fout.Filename);
+    if strcmpi(infile,outfile) && ~canceled_by_user
         [ok,msg,msgid]=movefile(tmpfile,infile);
         if ~ok
             error(msgid,msg);
         end
+    elseif canceled_by_user
+        delete(tmpfile)
     end
+        
 end
