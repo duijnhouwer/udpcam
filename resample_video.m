9d422754a337950c03f52443fc4274d2198464c9
Jacob Duijnhouwer <duijnhouwer@users.noreply.github.com>
2020-03-16 14:43:31 -0400

    Update resample_video.m
    Added a help section, updated some name,value name 'out' to 'outfile'

--- resample_video.m
+++ resample_video.m
@@ -1,16 +1,40 @@
 function resample_video(infile,tstamps,fps,profile,varargin)
     
-    % Nearest neighbor temporal resampling of video file
-    % If the infile is empty or the same as the outfile, the conversion
-    % will first stream to an intermediate file which will at end replace
-    % the infile. The intermediate file will then be replaced.
+    %resample_video - Nearest neighbor temporal resampling of video files
+    %
+    %   resample_video(infile,tstamps,fps,profile,varargin)
+    %
+    %   infile - filename of video file to be resampled
+    %   tstamps - array with timestamps at which the frames in infile were
+    %       collected (or any monotonically increasing array with a value
+    %       for each infile frame if you feel creative)
+    %   fps - the framerate of the output file
+    %   profile - a video profile, or the format of the output file. For
+    %       example 'MPEG-4' (Refer to help VideoWriter for a list of
+    %       profiles).
+    %
+    %   Optional Name,Value pairs: 
+    %
+    %   'outfile','' - optional output filename. If the outfile is not
+    %       specified or the same as the infile, the conversion will first
+    %       stream to an intermediate file which will at end replace the
+    %       infile. The intermediate file will then be replaced.
+    %   'progfun','' - Use this to show a progress indicator and a cancel
+    %       option. Optional handle to a custom function of the form
+    %       c=fun(i,n) that will be called before every video frame where i
+    %       is the input frame number about to resampled and n the total
+    %       number of frames in the input video, c is an output boolean
+    %       that indicates if the user requested cancelation of the
+    %       process.
+    %   
+    %   Jacob Duijnhouwer 2020-03-16
     
     p=inputParser;
     p.addRequired('infile',@ischar);
     p.addRequired('tstamps',@isnumeric); % frame times in seconds
-    p.addRequired('fps',@(x)and(isnumeric(x),x>0)); % output frames per seconds
+    p.addRequired('fps',@(x)and(isnumeric(x),x>0)); % desired frames rate of output file
     p.addRequired('profile',@ischar);
-    p.addParameter('out','',@ischar);
+    p.addParameter('outfile','',@ischar);
     p.addParameter('vidprops',struct,@isstruct);
     p.addParameter('progfun','',@(x)isa(x,'function_handle'));
     p.parse(infile,tstamps,fps,profile,varargin{:});
@@ -19,9 +43,9 @@ function resample_video(infile,tstamps,fps,profile,varargin)
         error('frame times array in seconds (arg #2) must be increasing monotonically');
     end
     
-    fin=VideoReader(infile);
+    fid_in=VideoReader(infile);
     
-    outfile=p.Results.out;
+    outfile=p.Results.outfile;
     if isempty(outfile)
         outfile=infile; % overwrite
     end
@@ -36,32 +60,32 @@ function resample_video(infile,tstamps,fps,profile,varargin)
             folder=pwd;
         end
         tmpfile=[tempname(folder) '_' mfilename];
-        fout=VideoWriter(tmpfile,profile);
+        fid_out=VideoWriter(tmpfile,profile);
     else
-        fout=VideoWriter(outfile,profile);
+        fid_out=VideoWriter(outfile,profile);
     end
     % Se the optional video properties from vidprops struct. which are
     % valid depends on the profile in use. MPEG-4 has Quality for example.
     % All have FrameRate, but see below
     flds=fieldnames(p.Results.vidprops);
     for i=1:numel(flds)
-        if fout.(flds{i})~=p.Results.vidprops.(flds{i})
+        if fid_out.(flds{i})~=p.Results.vidprops.(flds{i})
             % only write when different to prevent error like 'Setting the
             % CompressionRatio when LosslessCompression is enable is not
             % allowed.'
-            fout.(flds{i})=p.Results.vidprops.(flds{i});
+            fid_out.(flds{i})=p.Results.vidprops.(flds{i});
         end
     end
     % All video profiles have the settable property FrameRate. If it was in
     % vidprops it will now be overruled by required argumen fps
-    fout.FrameRate=fps;
+    fid_out.FrameRate=fps;
     
-    % Start writing the frames to out
-    open(fout);
+    % Start writing the frames to outfile
+    open(fid_out);
     try
-        % withing try-catch so we can close(out) upon error
-        n_out_frames=round(tstamps(end)*fout.FrameRate);
-        nearest_frame=readFrame(fin);
+        % within try-catch so we can close(fid_out) upon error
+        n_out_frames=round(tstamps(end)*fid_out.FrameRate);
+        nearest_frame=readFrame(fid_in);
         in_frame_counter=1;
         canceled_by_user=false;
         for i=1:n_out_frames
@@ -69,23 +93,23 @@ function resample_video(infile,tstamps,fps,profile,varargin)
             if canceled_by_user
                 break
             end
-            [~,nearest_neighbor]=min(abs(tstamps-i/fout.FrameRate));
+            [~,nearest_neighbor]=min(abs(tstamps-i/fid_out.FrameRate));
             if in_frame_counter<nearest_neighbor
                 in_frame_counter=nearest_neighbor;
-                nearest_frame=readFrame(fin); % get the next frame
+                nearest_frame=readFrame(fid_in); % get the next frame
             end
-            writeVideo(fout,nearest_frame); % add nearest frame to the output
+            writeVideo(fid_out,nearest_frame); % add nearest frame to the output
         end
     catch me
-        close(fout)
+        close(fid_out)
         rethrow(me)
     end
-    close(fout);
-    delete(fin); % free the handle (otherwise can't move file on top of it)
+    close(fid_out);
+    delete(fid_in); % free the handle (otherwise can't move file on top of it)
     
     % if the infile and the outfile were the same, replace original with
     % the resampled one. unless user canceled, then just delete it.
-    tmpfile=fullfile(fout.Path,fout.Filename);
+    tmpfile=fullfile(fid_out.Path,fid_out.Filename);
     if strcmpi(infile,outfile) && ~canceled_by_user
         [ok,msg,msgid]=movefile(tmpfile,infile);
         if ~ok
@@ -93,6 +117,5 @@ function resample_video(infile,tstamps,fps,profile,varargin)
         end
     elseif canceled_by_user
         delete(tmpfile)
-    end
-        
+    end      
 end
